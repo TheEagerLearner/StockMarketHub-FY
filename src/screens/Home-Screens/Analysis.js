43469ee95ac7fee58bf453ad8a95075c0359a3c9
TheEagerLearner <saurabhmul10@gmail.com>
2022-07-05 10:16:45 +0530

    configuring React-Native-charts

--- src/screens/Home-Screens/Analysis.js
+++ src/screens/Home-Screens/Analysis.js
@@ -1,10 +1,48 @@
 import React, { useEffect, useState } from 'react';
-import { View,StyleSheet,Text, AsyncStorage } from 'react-native';
+import { View,StyleSheet,Text, AsyncStorage,Dimensions  } from 'react-native';
 import StockApi from '../../features/StockApi/StockApi';
+
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+  } from "react-native-chart-kit";
+
 
 const Analysis = ({navigation}) => {
 
+
+    var newData = [];
     const [ticker,setTicker] = useState('');
+       
+    const screenWidth = Dimensions.get("window").width;
+
+    const arrangeData = (data) => {
+        const len = data.length;
+        let label = [];
+        let dataset = [];
+        for (var i = len-5; i<len; i++){
+            dataset = [...dataset,data[i].close];
+            label = [...label,data[i].date];
+
+        }   
+
+        newData = {
+            "datasets":[
+                {
+                "data":dataset
+            }
+        ],
+            "labels":label
+        }
+
+        console.log(newData);
+        
+
+    }
 
     const getStockHist = async (res) => {
 
@@ -12,6 +50,7 @@ const Analysis = ({navigation}) => {
             const response = await StockApi.get(`/stock/${res}/`);
             const data = response.data;
             console.log(data[0]); //gets all the stock historical data from the start date
+            arrangeData(data);
 
         }
         catch(err){
@@ -38,12 +77,30 @@ const Analysis = ({navigation}) => {
     useEffect(()=>{
         console.log("Analysis");
         getTicker();
+    
         
     },[]);
+  
+
+    const chartConfig = {
+        backgroundGradientFrom: "#1E2923",
+        backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: "#08130D",
+        backgroundGradientToOpacity: 0.5,
+        color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+        strokeWidth: 2, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: false // optional
+      };
 
     return(
         <View style={stylesheet.container}>
-            <Text>Analysis zai re ?</Text>
+            <LineChart
+                data={newData}
+                width={screenWidth}
+                height={220}
+                chartConfig={chartConfig}
+            />
         </View>
     );
 }
